abb4a1e4b044ccc97b6e219724da569564e58f58
Phi-phi <kame.kazu.0213.masumerry.0112@gmail.com>
2015-11-23 22:04:56 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :verify, only: [:index]
+  post '/verify' => 'verify#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
