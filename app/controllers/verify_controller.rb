3b7d86a12f73e033a7c2cf2cfc7a18489dd02d97
Phi-phi <kame.kazu.0213.masumerry.0112@gmail.com>
2015-11-23 21:46:01 +0900

    コントローラーまわり実装（リターンはまだ）、ルーティング変更

--- app/controllers/verify_controller.rb
+++ app/controllers/verify_controller.rb
@@ -1,2 +1,33 @@
 class VerifyController < ApplicationController
+  def index
+    @user = User.where(macaddr: user_params[:my_addr])
+    @now_ap = AccessPoint.where(macaddr: user_params[:ap_addr]) 
+    if @user.blank?
+      redirect_to action: 'create'
+    else
+      redirect_to action: 'ap_check'
+    end
+  end
+
+  def ap_check
+    past_ap = @user.access_point
+
+    if past_ap.macaddr != @now_ap.macaddr
+      @user.access_point_id = @now_ap.id
+      @user.save
+    end
+  end
+
+  def create
+    @user = User.new(
+      :macaddr => user_params[:my_addr],
+      :access_point_id => @now_ap.id
+    )
+    @user.save
+  end
+
+  private
+  def user_params
+    params.require(:check).permit(:my_addr, :ap_addr)
+  end
 end
