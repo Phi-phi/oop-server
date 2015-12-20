f8cd7f1b96ce1c13fbd80839ce1a104c0dcac11f
Todoroki <todoroki.auone@gmail.com>
2015-12-20 15:04:09 +0900

    wrote several tests

--- spec/controllers/document_controller_spec.rb
+++ spec/controllers/document_controller_spec.rb
@@ -2,4 +2,28 @@ require 'rails_helper'
 
 RSpec.describe DocumentController, :type => :controller do
 
+  # サンプルAPIリファレンス
+  describe "GET /dev/api/reference/post/account/register" do
+
+    # HTTP 200を返すことをテスト
+    it "responds successfully with an HTTP 200 status code" do
+      get :api_reference
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    # api_reference テンプレートをレンダリングすることをテスト
+    it "renders the index template" do
+      get :api_reference
+      expect(response).to render_template("api_reference")
+    end
+
+    # ページタイトルになるものが正しいかテスト
+    specify "the title should be 'POST account/register'" do
+      get :api_reference
+      api = Api.find_by(name:"POST account/register")
+      expect(api).to be_present
+    end
+
+  end
 end
--- spec/models/api_spec.rb
+++ spec/models/api_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Api, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  %w(name description resource_url).each do |col|
+    it "isn't valid without #{col}" do
+      api = Api.new(
+          name: 'sample name',
+          description: 'hoge hoge',
+          resource_url: 'account/sample.json'
+      )
+      api[col] = nil
+      expect(api).not_to be_valid
+
+      api.save
+      expect(api.errors[:name]).to be_present
+    end
+  end
 end
